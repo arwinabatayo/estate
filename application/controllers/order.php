62f4aa4626af31507d36080d52e2828358d04d26
Mary Ranz Gellie Aquino <mrgiaquino@gmail.com>
2013-09-13 17:52:23 +0800

    used updated estate_orders and estate_order_items

--- application/controllers/order.php
+++ application/controllers/order.php
@@ -1,20 +1,20 @@
-<?php 
+<?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Controller/Handler for: 
+ * Controller/Handler for:
  *  -- showing status of order
- * 
+ *
  ***/
 
-class Order extends MY_Controller 
+class Order extends MY_Controller
 {
 	var $_data = null;
-	
+
 	function __construct()
 	{
 		parent::__construct();
-		
+
         //global data
 		$this->_data->tpl_view    = $this->config->item('globe_estate_template_path');
 		$this->_data->assets_path = $assets_path = $this->config->item('globe_estate_assets');
@@ -28,20 +28,24 @@ class Order extends MY_Controller
 
 		$this->load->model('estate/orderitem_model');
 		$this->load->model('estate/accounts_model');
+		$this->load->model('estate/order_model');
 	}
-	
+
 	public function index()
-	{	
-		// get refnum from url
+	{
+		// get refnum / ordernum from url
 		$refnum = $this->input->get('refnum', TRUE);
 
-		// send orders to view
-		$this->_data->orders = $this->orderitem_model->get_orderitems_by_orderid($refnum);
+		// send order detals to view
+		$this->_data->order = $this->order_model->get_order_by_refnum($refnum);
+
+		// send order item details to view
+		$this->_data->order_items = $this->orderitem_model->get_orderitems_by_orderid($refnum);
 
 		//TODO - move to session after authentication
 
 		$this->_data->account = $this->accounts_model->get_account_info_by_id('09173858958', false);
-		
+
 		$this->load->view($this->_data->tpl_view, $this->_data);
 	}
 
