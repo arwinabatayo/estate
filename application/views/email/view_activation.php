18c2227548d689de5c9dcebd3b50de7e447ec900
stephen moraga <smoraga@live.com>
2013-09-16 12:38:27 +0800

    send_email - API

--- application/views/email/view_activation.php
+++ application/views/email/view_activation.php
@@ -1,13 +1,14 @@
-<p>Dear <?php echo $name; ?>,</p>
-<br>
-<p>Welcome!</p>
+Dear <?php echo $name; ?>,
 
-<p>Thanks for signing up, we just need to verify your email address:</p>
+Welcome!
 
-<p>Click <a href="<?php echo $verification_url; ?>">here</a> or you may copy the url provided below to verify you account.</p>
-<p><?php echo $verification_url; ?></p>
-<br>
-<p>Thank You!</p>
-<p>The Project Estate Team</p>
-<br><br>
-<p style="color:#555"><em>*** Note: This is an automated email.  Do NOT reply to this message.</em></p>
+Thanks for signing up, we just need to verify your email address:
+
+You may copy or click the url provided below to verify you account.
+<?php echo $verification_url; ?>
+
+
+Thank You!
+The Project Estate Team
+
+*** Note: This is an automated email.  Do NOT reply to this message.
