c89adecf53c3d4ef42d8438bc196cf7aa5132138
Mervin Novera <mervin@filament.ph>
2013-09-16 20:10:21 +0800

    updated application/views/admin/view_ecommerce.php

--- application/views/admin/view_ecommerce.php
+++ application/views/admin/view_ecommerce.php
@@ -10,32 +10,32 @@
 	
 	<table class="g_table zebra">
 		<tr>
-			<td width="53" align="right">
-				<a 	href="<?php echo base_url(); ?>admin/products" class="g_tableicon">Products</a>
+			<td align="left">
+				<a 	href="<?php echo base_url(); ?>admin/products" class="">Products</a>
 				<div class="h_clearboth"></div>
 			</td>
 		</tr>
 		<tr>
-			<td width="53" align="right">
-				<a 	href="<?php echo base_url(); ?>admin/accessories" class="g_tableicon">Accessories</a>
+			<td align="left">
+				<a 	href="<?php echo base_url(); ?>admin/accessories" class="">Accessories</a>
 				<div class="h_clearboth"></div>
 			</td>
 		</tr>
 		<tr>
-			<td width="53" align="right">
-				<a 	href="<?php echo base_url(); ?>admin/addonscategories" class="g_tableicon">Addons categories</a>
+			<td align="left">
+				<a 	href="<?php echo base_url(); ?>admin/addonscategories" class="">Addons categories</a>
 				<div class="h_clearboth"></div>
 			</td>
 		</tr>
 		<tr>
-			<td width="53" align="right">
-				<a 	href="<?php echo base_url(); ?>admin/packageplans" class="g_tableicon">Package Plans</a>
+			<td align="left">
+				<a 	href="<?php echo base_url(); ?>admin/packageplans" class="">Package Plans</a>
 				<div class="h_clearboth"></div>
 			</td>
 		</tr>
 		<tr>
-			<td width="53" align="right">
-				<a 	href="<?php echo base_url(); ?>admin/userfunctions" class="g_tableicon">User functions</a>
+			<td align="left">
+				<a 	href="<?php echo base_url(); ?>admin/userfunctions" class="">User functions</a>
 				<div class="h_clearboth"></div>
 			</td>
 		</tr>
