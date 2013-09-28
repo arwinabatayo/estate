d94eccf173fddde2ea9c339926e0ee3fbded4587
Jason Burgos <xerenader@gmail.com>
2013-09-28 20:54:16 +0800

    Updates for Create Your Own Plan Template

--- application/views/globe-estate-blue/sections/pages/page_plan_create.php
+++ application/views/globe-estate-blue/sections/pages/page_plan_create.php
@@ -18,7 +18,175 @@
 												<label>Create your own plan by selecting your own Combos.</label>
 						
 												<div class="main" style="width:100%;">
-													
+													<ul id="og-grid" class="og-grid">
+                                            <li>
+                                                <a href="#" data-largesrc="" 
+                                                    data-title="
+														<i class='icon icon-big-coins'></i>
+                                                        Get 500 Peso Value/Month
+                                                        "
+                                                data-description="CASHOUT P500.00">
+                                                    <div class="plan-tile-option-pink">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new hide"></div>
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">PLAN 299</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 500 PV/mo.</div>
+                                                            <div class="plan-off hide"></div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" data-largesrc="" 
+                                                    data-title="
+                                                        <i class='icon icon-big-coins'></i>
+                                                        Get 900 Peso Value/Month
+                                                        "                                               
+                                                data-description="CASHOUT P900.00" >
+                                                    <div class="plan-tile-option-pink ">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new hide"></div>
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">PLAN 499</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 900 PV/mo.</div>
+                                                            <div class="plan-off hide"></div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a  href="#" data-largesrc="" 
+                                                    data-title="
+                                                        <i class='icon icon-big-coins'></i>
+                                                        Get 1,700 Peso Value/Month
+                                                        "
+                                                data-description="CASHOUT P1,700.00">
+                                                    <div class="plan-tile-option-pink">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new hide"></div>
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">PLAN 999</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 1700 PV/mo.</div>
+                                                            <div class="plan-off hide"></div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            
+                                            <li>
+                                                <a  href="#" data-largesrc="" 
+                                                    data-title="
+                                                        <i class='icon icon-big-coins'></i>
+                                                        Get 3,200 Peso Value/Month
+                                                        "
+                                                data-description="CASHOUT P3,200.00">
+                                                    <div class="plan-tile-option-pink">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new hide"></div>
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">PLAN 1799</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 3200 PV/mo.</div>
+                                                            <div class="plan-off hide"></div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a  href="#" data-largesrc="" 
+                                                    data-title="
+                                                        <i class='icon icon-big-coins'></i>
+                                                        Get 12,500 Peso Value/Month
+                                                        "
+                                                data-description="CASHOUT P12,500.00">
+                                                    <div class="plan-tile-option-pink">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new hide"></div>
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">PLAN 2499</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 4400 PV/mo.</div>
+                                                            <div class="plan-off hide"></div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                    
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a  href="#" data-largesrc="" 
+                                                    data-title="
+                                                        <i class='icon icon-big-coins'></i>
+                                                        Get 6,500 Peso Value/Month
+                                                        "                                               
+                                                data-description="CASHOUT P6,500.00">
+
+                                                    <div class="plan-tile-option-pink plan-info-height">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new hide"></div>
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">PLAN 3799</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 6500 PV/mo.</div>
+                                                            <div class="plan-off hide"></div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                    
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a  href="#" data-largesrc="" 
+                                                    data-title="
+                                                        <i class='icon icon-big-coins'></i>
+                                                        Get 200 Peso Value/Month
+                                                        "
+                                                data-description="CASHOUT P200.00">
+                                                    <div class="plan-tile-option-pink">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new hide"></div>
+                                                        
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">UNLI PLAN</div>
+                                                            <div class="">Pick and mix Boosters<br />of your choice</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 200 PV/mo.</div>
+                                                            <div class="plan-off hide"></div>
+                                                        </div>
+                                                        
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a  href="#" data-largesrc="" 
+                                                    data-title="
+                                                        <i class='icon icon-big-coins'></i>
+                                                        Get 200 Peso Value/Month
+                                                        " 
+                                                    data-description="CASHOUT P200.00">
+                                                    <div class="plan-tile-option-pink">
+                                                        <div class="arrow-point-up"></div>
+                                                        <div class="ribbon-new"></div>
+                                                        <div class="center">
+                                                            <i class="icon icon-peso"></i>
+                                                            <div class="plan-name">FAMILY PLAN</div>
+                                                            <div class="">Group 3 up to 11<br />Unli Plans</div>
+                                                            <div class="">get <i class="icon icon-coins"></i> 200 PV/mo.</div>
+                                                            <div class="plan-off"><b>P100 OFF</b> per unli plan</div>
+                                                        </div>
+                                                    </div>
+                                                </a>
+                                            </li>
+                                            
+                                        </ul>
 													<div class="clr"></div>
 												</div>
 											
