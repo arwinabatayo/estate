9e2c16c203ed41c0b2be8422cc9bd54f818212bf
zephyranz <maquino@Geromes-iMac.local>
2013-10-02 19:51:53 +0800

    revisions with email tempalte

--- application/views/email/view_transactionlink.php
+++ application/views/email/view_transactionlink.php
@@ -1,11 +1,11 @@
-<p>Hi <?php echo $name; ?>,</p>
-<br>
-<p>Welcome!</p>
+Hi <?php echo $name; ?>,
 
-<p>You can resume your uncompleted transaction by visiting  <a href="<?php echo $link; ?>">here</a> or you may copy the url provided below to resume.</p>
-<p><?php echo $link; ?></p>
-<br>
-<p>Thank You!</p>
-<p>The Project Estate Team</p>
-<br><br>
-<p style="color:#555"><em>*** Note: This is an automated email.  Do NOT reply to this message.</em></p>
+Welcome!
+
+You can resume your uncompleted transaction by visiting <?php echo $link; ?> or copy the url provided below on your browser to resume.
+<?php echo $link; ?>
+
+Thank You!
+The Project Estate Team
+
+*** Note: This is an automated email.  Do NOT reply to this message.
